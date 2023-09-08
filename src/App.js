f09512a887a859be01c1e874e5cc79413ebebfaf
Rangasamy <rangasamy.psg@gmail.com>
2023-09-08 17:52:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Navbar from './pages/Navbar';
+
+import ViewStudent from './pages/ViewStudent';
+import AddStudent from './pages/AddStudent';
+import EditStudent from './pages/EditStudent';
+
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:8000/";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+
+          <Navbar />
+
+          <Switch>
+
+            <Route path="/students" component={ViewStudent} />
+            <Route path="/add-student" component={AddStudent} />
+            <Route path="/edit-student/:id" component={EditStudent} />
+
+          </Switch>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
